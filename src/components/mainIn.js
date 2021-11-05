2f4eeeb7eaa1c47fa95d7157c42691b7583c809c
wang <wangy@PJNTPB-JA01>
2021-11-05 18:03:53 +0900

    employee

--- src/components/mainIn.js
+++ src/components/mainIn.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
-import LoginManager from './loginManager';
-import Gate from './gate';
 import LoginEmployee from './loginEmployee';
-import SubMenuManager from './subMenuManager';
 import SubMenuEmployee from './subMenuEmployee';
 import axios from 'axios';
 axios.defaults.withCredentials = true;
@@ -16,9 +13,7 @@ class mainIn extends Component {
             <Router>
                 <>
                     <Route exact path="/" component={LoginEmployee} />
-                    <Route path="/loginManager" component={LoginManager} />
                     <Route path="/loginEmployee" component={LoginEmployee} />
-                    <Route path="/subMenuManager" component={SubMenuManager} />
                     <Route path="/subMenuEmployee" component={SubMenuEmployee} />
                 </>
             </Router>
